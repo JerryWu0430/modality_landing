23dd6723ee903fd29685516fd7bb6cb67c97ac19
Jerry <woohaoran@gmail.com>
2025-06-16 15:18:52 +0200

    init true

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Header from './components/layout/Header';
-import Hero from './components/sections/Hero';
+import { HeroSection } from '@/components/ui/hero-section-1'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Header />
-      <Hero />
+      <HeroSection />
+      
+
     </main>
-  );
+  )
 }
