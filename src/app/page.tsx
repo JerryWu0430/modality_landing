d60eaf9b8999d0f8192e33899561832db45c3d98
Jerry <woohaoran@gmail.com>
2025-06-17 17:15:01 +0200

    minimal structural refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-<<<<<<< dashboard
 import { HeroSection } from '@/app/sections/hero-section'
-import { PricingSection } from '@/app/sections/pricing-section'
-
-=======
-import { HeroSection } from '@/components/sections/hero-section'
-import { PricingSection } from '@/components/sections/pricing-section'
-import { PricingCardsSection } from '@/components/sections/pricing-cards-section'
->>>>>>> main
+import { PricingCardsSection } from '@/app/sections/pricing-cards-section'
 
 export default function Home() {
   return (
