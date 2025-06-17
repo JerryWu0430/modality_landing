d551cc3feec703b7e833a140d0f5829454fbe112
Jerry <woohaoran@gmail.com>
2025-06-17 16:48:50 +0200

    updated pricing

---
+++ src/components/sections/pricing-cards-section.tsx
@@ -0,0 +1,9 @@
+import { Pricing } from '@/components/ui/pricing-cards'
+
+export function PricingCardsSection() {
+  return (
+    <section id="pricing-cards" className="w-full bg-background">
+      <Pricing />
+    </section>
+  )
+} 
